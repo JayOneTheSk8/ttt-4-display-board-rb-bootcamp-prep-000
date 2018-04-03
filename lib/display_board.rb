7e15c8fd5e3c2b6bc42e471e5a97191a42f30444
Justin Cox <misterjustincox@gmail.com>
2018-04-03 10:04:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,9 @@
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
-  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-  div1 = "-----------"
-  div2 = "-----------"
-  puts row1
-  puts div1
-  puts row2
-  puts div2
-  puts row3
+  rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "]
+  puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
 end
