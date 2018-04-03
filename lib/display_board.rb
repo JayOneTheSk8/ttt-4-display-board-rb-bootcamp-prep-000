6cb58ac57b673d841d83db541e8541d8e7593265
Justin Cox <misterjustincox@gmail.com>
2018-04-03 10:05:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
 def display_board(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   rows = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "]
   puts rows[0]
   puts rows[1]
